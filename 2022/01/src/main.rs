f0f2515373db5f7e87e23699e4f0483a675ff97e
dxlnr <daniel.illner@outlook.de>
2023-05-18 14:36:18 +0200

    01 part one correct

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs;
 use std::collections::HashMap;
 
@@ -7,16 +6,48 @@ fn read_file_string(filepath: &str) -> Result<String, Box<dyn std::error::Error>
     Ok(data)
 }
 
-fn find_calories(s: String) -> Option<HashMap<i32, i32>> {
+fn find_calories(s: String) -> HashMap<i32, i32> {
     let mut r: HashMap<i32, i32>  = HashMap::new(); 
-    let mut cs = s.split('\n').collect::<Vec<&str>>();
+    let cs: Vec<Option<i32>> = s.split('\n').map(|s| s.parse::<i32>().ok()).collect();
 
-    println!("{:?}", &cs);
-    None
+    let mut idx = 1;
+    let mut calories = 0;
+    for c in cs.iter() {
+        match c {
+            None => {
+                idx = idx + 1;
+                calories = 0;
+            },
+            Some(i) => {
+                calories = calories + i;
+                r.insert(idx, calories);
+            }
+        }
+    }
+    r
 }
 
-fn main() {
+fn max<K, V>(hm: &HashMap<K, V>) -> Option<(&K, &V)>
+where
+    V: Ord,
+{
+   hm 
+        .iter()
+        .max_by(|a, b| a.1.cmp(&b.1))
+        .map(|(k, v)| (k, v))
+}
 
-    let ds = read_file_string("input");
-    _ = find_calories(ds.unwrap());
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ds = read_file_string("puzzle")?;
+    let rmap = find_calories(ds);
+    let r = max(&rmap);
+
+    match r {
+            None => { println!("No elf found."); },
+            Some(i) => {
+                println!("(Elf, calories) {:?}", &i);
+            },
+        }
+
+    Ok(())
 }
