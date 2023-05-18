e6be4825397c2cb4e1cbf258fdd77af10725c3a6
dxlnr <daniel.illner@outlook.de>
2023-05-18 15:02:28 +0200

    01 part two correct

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,13 +1,13 @@
 use std::fs;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 fn read_file_string(filepath: &str) -> Result<String, Box<dyn std::error::Error>> {
     let data = fs::read_to_string(filepath)?;
     Ok(data)
 }
 
-fn find_calories(s: String) -> HashMap<i32, i32> {
-    let mut r: HashMap<i32, i32>  = HashMap::new(); 
+fn find_calories(s: String) -> BTreeMap<i32, i32> {
+    let mut r: BTreeMap<i32, i32>  = BTreeMap::new(); 
     let cs: Vec<Option<i32>> = s.split('\n').map(|s| s.parse::<i32>().ok()).collect();
 
     let mut idx = 1;
@@ -15,11 +15,11 @@ fn find_calories(s: String) -> HashMap<i32, i32> {
     for c in cs.iter() {
         match c {
             None => {
-                idx = idx + 1;
+                idx += 1;
                 calories = 0;
             },
             Some(i) => {
-                calories = calories + i;
+                calories += i;
                 r.insert(idx, calories);
             }
         }
@@ -27,27 +27,48 @@ fn find_calories(s: String) -> HashMap<i32, i32> {
     r
 }
 
-fn max<K, V>(hm: &HashMap<K, V>) -> Option<(&K, &V)>
+fn max<K, V>(hm: &BTreeMap<K, V>) -> Option<(&K, &V)>
 where
     V: Ord,
 {
    hm 
         .iter()
-        .max_by(|a, b| a.1.cmp(&b.1))
+        .max_by(|a, b| a.1.cmp(b.1))
         .map(|(k, v)| (k, v))
 }
+
+fn sort<K, V>(hm: &BTreeMap<K, V>) -> Vec<(&K, &V)>
+where
+    V: Ord,
+{
+    let mut v = Vec::from_iter(hm);
+    v.sort_by(|&(_, a), &(_, b)| b.cmp(a));
+    v
+}
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ds = read_file_string("puzzle")?;
     let rmap = find_calories(ds);
-    let r = max(&rmap);
 
-    match r {
-            None => { println!("No elf found."); },
-            Some(i) => {
-                println!("(Elf, calories) {:?}", &i);
-            },
-        }
+    //  --- Part One ---
+    let r1 = max(&rmap);
+    match r1 {
+        None => { println!("No elf found."); },
+        Some(i) => {
+            println!("(Elf, calories) {:?}", &i);
+        },
+    }
+
+    //  --- Part Two ---
+    let r2 = sort(&rmap);
+    println!("(Elf, calories) {:?}", &r2[..3]);
+
+    let mut sum = 0;
+    for i in &r2[..3] {
+        sum += i.1;
+    }
+    println!("\n\tTotal sum : {:?}", sum);
 
     Ok(())
 }
