476f2e2877288ac55c56fb53d8aae880402af2ea
dxlnr <daniel.illner@outlook.de>
2023-12-10 19:00:45 +0100

    some 03 prog

--- 2023/03/app/Main.hs
+++ 2023/03/app/Main.hs
@@ -2,32 +2,77 @@ module Main where
 import System.Environment (getArgs)
 import System.IO (openFile, IOMode(ReadMode), hGetContents)
 import Data.List.Split (splitOn)
-import Text.Regex (matchRegex)
-import Text.Regex.TDFA 
-import Text.Regex.TDFA.String
+import Text.Regex (matchRegex, mkRegex, Regex, matchRegexAll)
 
-checkSym :: String -> [String]
-checkSym input = getAllTextMatches $ input =~ "[0-9]+|[*]|[#]|[$]" :: [String]
+escapeRegex :: String -> String
+escapeRegex = concatMap escapeChar
+  where
+    escapeChar c
+      | c `elem` "\\+*?[^]$(){}=!<>|:-" = ['\\', c]
+      | otherwise = [c]
 
--- [0-9]+|[*]|[#]|[$]
-extractNumbers :: String -> [String]
-extractNumbers input = getAllTextMatches $ input =~ "[0-9]+" :: [String]
+findSubstrIdx :: String -> String -> (Int, Int)
+findSubstrIdx substr str = case matchRegexAll (mkRegex substr) str of
+    Nothing -> (-1, -1)
+    Just (pre, match, _, _) -> (length pre, length pre + length match - 1)
 
-isDot :: Char -> Bool
-isDot char = char == '.' 
+findSymIdx :: String -> String -> Int
+findSymIdx substr str = case matchRegexAll (mkRegex (escapeRegex substr)) str of
+    Nothing -> -1
+    Just (pre, _, _, _) -> length pre
 
-findDots :: [Char] -> [Bool]
-findDots x = map isDot x  
+matchAll :: Regex -> String -> [(String, (Int, Int))]
+matchAll regex s = go s
+  where
+    go str = case matchRegexAll regex str of
+        Nothing -> []
+        Just (pre, match, rest, _) -> (match, findSubstrIdx match s) : go rest
 
-prog :: String -> [[Bool]]
+matchAllSym :: String -> String -> [Int]
+matchAllSym pattern input = go input
+  where
+    regex = mkRegex pattern
+    go s = case matchRegexAll regex s of
+        Nothing -> []
+        Just (_, match, rest, _) -> findSymIdx match input : go rest
+        -- Just (_, match, rest, _) -> match : go rest
+
+findAllNumbersWithIndices :: String -> [(String, (Int, Int))]
+findAllNumbersWithIndices input = matchAll (mkRegex "[0-9]+") input
+
+findAllSymWithIndices :: String -> [Int]
+findAllSymWithIndices input = matchAllSym "[*#$+]" input
+
+prog :: String -> [[(String, (Int, Int))]]
 prog x = let lines = splitOn "\n" x
-         in map findDots lines
+         in map findAllNumbersWithIndices lines
+
+progSym :: String -> [[Int]]
+progSym x = let lines = splitOn "\n" x
+         in map findAllSymWithIndices lines
+
+compareElements :: (a -> b -> [String]) -> [a] -> [b] -> [[String]]
+compareElements f l1 l2 = zipWith f l1 l2
+
+withinRange :: [(String, (Int, Int))] -> [Int] -> [String]
+withinRange ranges ints = map (rangeString . findRange) ints
+  where
+    findRange i = filter (\(_, (low, high)) -> i >= low - 1 && i <= high + 1) ranges
+    rangeString ((s, _):_) = s
+    rangeString _ = "empty"
 
 main :: IO ()
 main = do
     args <- getArgs
     file <- openFile (head args) ReadMode
     schematic <- hGetContents file
-    print schematic 
 
-    print $ prog schematic
+    let ns = prog schematic
+    let syms = progSym schematic
+
+    let res = compareElements withinRange ns syms
+
+    -- print schematic 
+    -- print ns
+    -- print syms
+    print res
