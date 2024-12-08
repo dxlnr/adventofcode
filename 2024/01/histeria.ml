2cbef780d42804bcc96cfdc128cb5ad9b5c59d1e
dxlnr <daniel.illner@outlook.de>
2024-12-09 00:45:48 +0100

    one is solved

--- 2024/01/histeria.ml
+++ 2024/01/histeria.ml
@@ -1,39 +1,47 @@
-let file = "test1"
-
-let read fn = 
-    let ic = open_in file in
-    (* open_in: string -> in_channel *)
-    let lines = ref [] in
-    try
-        try
-            while true; do
-                lines := input_line ic :: !lines 
-            done;
-        with End_of_file ->
-            close_in ic;
-
-        let content = List.rev !lines in
-        content
-    with e ->
-        close_in_noerr ic;
-        raise e
+let file = "input"
 
 let convert_to_pair lst =
   match lst with
-  | [x; y] -> (x, y)
+  | [ x; y ] -> (x, y)
   | _ -> failwith "Converting failed: List must have exactly 2 elements"
 
+let rec suml lst =
+  match lst with
+  | [] -> 0
+  | [ x ] -> x
+  | head :: second :: tail -> suml ((head + second) :: tail)
+
+let read fn =
+  (* open_in: string -> in_channel *)
+  let ic = open_in file in
+  let lines = ref [] in
+  try
+    try
+      while true do
+        lines := input_line ic :: !lines
+      done
+    with End_of_file ->
+      close_in ic;
+
+      let content = List.rev !lines in
+      content
+  with e ->
+    close_in_noerr ic;
+    raise e
+
 let splitlines content =
-    let lines = List.map(fun x -> convert_to_pair (List.map int_of_string (Str.split (Str.regexp "[ \t]+") x))) content in
-    let (a, b) = List.split lines in
-    (a, b)
+  let lines =
+    List.map
+      (fun x ->
+        convert_to_pair
+          (List.map int_of_string (Str.split (Str.regexp "[ \t]+") x)))
+      content
+  in
+  let a, b = List.split lines in
+  (a, b)
 
-let () = 
-    let content = read file in
-    let (a, b) = splitlines content in
+let () =
+  let content = read file in
+  let a, b = splitlines content in
 
-    let a = Quicksort.quicksort a 0 5 in
-    let b = Quicksort.quicksort b 0 5 in
-        
-    print_endline (String.concat "," (List.map string_of_int a));
-    print_endline (String.concat "," (List.map string_of_int b));
+  print_endline ""
--- 2024/01/quicksort.ml
+++ 2024/01/quicksort.ml
@@ -1,36 +1,29 @@
-let swap arr i j = 
-    let a = List.nth arr i in
-    let arr = List.mapi (fun idx x -> 
-        if idx = i then (List.nth arr j) 
-        else if idx = j then a 
-        else x) arr in
-    arr
-;;
-let partition arr st en = 
-    let i = ref (st - 1) in
-    let arr = ref arr in
-    for idx = st to en do
-        if List.nth !arr idx < List.nth !arr en then 
-            begin 
-                i := !i + 1; 
-                arr := swap !arr !i idx;
-            end;
-    done;
-    i := !i + 1; 
-    arr := swap !arr !i en;
-    (!arr, !i)
-;;
+let swap arr i j =
+  let a = List.nth arr i in
+  let arr =
+    List.mapi
+      (fun idx x ->
+        if idx = i then List.nth arr j else if idx = j then a else x)
+      arr
+  in
+  arr
 
-let rec quicksort arr st en = 
-    if st < en then
-        let (arr, pivot) = partition arr st en in
-        let arr = quicksort arr st (pivot - 1) in
-        let arr = quicksort arr (pivot + 1) en in
-        arr
-    else
-        arr
-;;
-(* let () = *) 
-(*     let test = quicksort [8;2;4;7;1;3;9;6;5] 0 8 in *)
-(*     print_endline (String.concat "," (List.map string_of_int test));; *)
-(* ;; *)
+let partition arr st en =
+  let i = ref (st - 1) in
+  let arr = ref arr in
+  for idx = st to en do
+    if List.nth !arr idx < List.nth !arr en then (
+      i := !i + 1;
+      arr := swap !arr !i idx)
+  done;
+  i := !i + 1;
+  arr := swap !arr !i en;
+  (!arr, !i)
+
+let rec quicksort arr st en =
+  if st < en then
+    let arr, pivot = partition arr st en in
+    let arr = quicksort arr st (pivot - 1) in
+    let arr = quicksort arr (pivot + 1) en in
+    arr
+  else arr
