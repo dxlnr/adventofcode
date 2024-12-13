7e1a7ff9488148a76386113ef03d037e0ed0bf52
dxlnr <daniel.illner@outlook.de>
2024-12-13 17:17:02 +0100

    day 2 solved

--- 2024/02/main.ml
+++ 2024/02/main.ml
@@ -22,85 +22,57 @@ let read fn =
 let match_level diff =
   match diff with x when x < 0 -> Dec | 0 -> Neither | _ -> Inc
 
-let rec check lst lvl system =
-  match lst with
-  | [] -> false
-  | [ x ] -> system
-  | h :: s :: tail ->
-      let diff = int_of_string h - int_of_string s in
-      if abs diff < 1 || abs diff > 3 then false
-      else
-        let nlvl = match_level diff in
-        if lvl == Neither || nlvl == lvl then
-          check (List.append [ s ] tail) nlvl true
-        else false
+let check lst =
+  let rec checkh l lvl system =
+    match l with
+    | [] -> false
+    | [ x ] -> system
+    | h :: s :: tail ->
+        let diff = int_of_string h - int_of_string s in
+        if abs diff < 1 || abs diff > 3 then false
+        else
+          let nlvl = match_level diff in
+          if lvl == Neither || nlvl == lvl then
+            checkh (List.append [ s ] tail) nlvl true
+          else false
+  in
+  checkh lst Neither true
 
-let rec check_w_tolerate f lst lvl system=
-  match lst with
-  | h :: s :: tail -> check_w_tolerate (check (List.append [ h ] tail) lvl system)
+let rec split_at i lst =
+  match (i, lst) with
+  | _, [] -> ([], [])
+  | 0, _ -> ([], lst)
+  | _, x :: xs ->
+      let left, right = split_at (i - 1) xs in
+      (x :: left, right)
 
-let rec check_w_tolerate f lst lvl system =
-  match lst with
-  | [] -> false (* Base case: no more elements to check *)
-  | h :: tail ->
-      if f (List.append tail []) lvl system then
-        true (* If `f` succeeds with the current element excluded, return true *)
-      else
-        check_w_tolerate f (tail @ [h]) lvl system
+let popl lst = match lst with [] -> [] | _ :: xs -> xs
 
+let check_w_tolerate lst =
+  let rec check_w_tolerate_helper i lst =
+    if i >= List.length lst then false
+    else
+      let left, right = split_at i lst in
+      if check (left @ popl right) then true
+      else check_w_tolerate_helper (i + 1) lst
+  in
+  check_w_tolerate_helper 0 lst
 
-  (* | [] -> false *)
-  (* | [ x ] -> system *)
-  (* | h :: s :: tail -> *)
-  (*     let diff = int_of_string h - int_of_string s in *)
-  (*     if abs diff < 1 || abs diff > 3 then false *)
-  (*     else *)
-  (*       let nlvl = match_level diff in *)
-  (*       if lvl == Neither || nlvl == lvl then *)
-  (*         check (List.append [ s ] tail) nlvl true *)
-  (*       else false *)
-
-(* let rec check_w_tolerate lst lvl system errc = *)
-(*   match lst with *)
-(*   | [] -> false *)
-(*   | [ x ] -> system *)
-(*   | h :: s :: tail -> *)
-(*       let diff = int_of_string h - int_of_string s in *)
-(*       if abs diff < 1 || abs diff > 3 then *)
-(*         if errc == 0 then *)
-(*           check_w_tolerate (List.append [ h ] tail) lvl false (errc + 1) *)
-(*         else false *)
-(*       else *)
-(*         let nlvl = match_level diff in *)
-(*         if lvl == Neither || nlvl == lvl then *)
-(*           check_w_tolerate (List.append [ s ] tail) lvl true errc *)
-(*         else if errc == 0 then *)
-(*           check_w_tolerate (List.append [ h ] tail) lvl false (errc + 1) *)
-(*         else false *)
-
-let rec countl lst c =
-  match lst with
-  | [] -> c
-  | x :: tail -> if x then countl tail (c + 1) else countl tail c
+let countl lst =
+  let rec countlh lst c =
+    match lst with
+    | [] -> c
+    | x :: tail -> if x then countlh tail (c + 1) else countlh tail c
+  in
+  countlh lst 0
 
 let () =
   let content = read file in
 
   let lst = List.map (fun s -> Str.split (Str.regexp "[ \\d]+") s) content in
 
-  let r1 = countl (List.map (fun x -> check x Neither false) lst) 0 in
+  let r1 = countl (List.map (fun x -> check x) lst) in
+  let r2 = countl (List.map (fun x -> check_w_tolerate x) lst) in
 
-  (* let res = countl (List.map (fun x -> check x Neither false) lst) 0 in *)
-  (* let lst = List.map (fun x -> check_w_tolerate x Neither false 0) lst in *)
-  let r2 = countl (List.map (fun x -> check_w_tolerate check x Neither false) lst) 0 in
-  (* let r2 = countl (List.map (fun x -> begin *) 
-  (*   let safe = check x Neither false in end ) lst) *)
-        (* check x Neither false) lst) 0 in *)
-
-  (* List.iteri (fun idx, x -> print_endline String.concat " : "  (string_of_bool x)) lst *)
-        (* List.iteri (fun idx x -> *) 
-  (* print_endline (String.concat " : " [string_of_int (idx +1); string_of_bool x]) *)
-(* ) lst *)
-
-    print_endline (string_of_int r1);
-    print_endline (string_of_int r2);
+  print_endline (string_of_int r1);
+  print_endline (string_of_int r2)
