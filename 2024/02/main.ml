2af96c24f6e19ad68065e4f4d3507023e8af1d57
dxlnr <daniel.illner@outlook.de>
2024-12-13 01:41:07 +0100

    fail

--- 2024/02/main.ml
+++ 2024/02/main.ml
@@ -35,6 +35,49 @@ let rec check lst lvl system =
           check (List.append [ s ] tail) nlvl true
         else false
 
+let rec check_w_tolerate f lst lvl system=
+  match lst with
+  | h :: s :: tail -> check_w_tolerate (check (List.append [ h ] tail) lvl system)
+
+let rec check_w_tolerate f lst lvl system =
+  match lst with
+  | [] -> false (* Base case: no more elements to check *)
+  | h :: tail ->
+      if f (List.append tail []) lvl system then
+        true (* If `f` succeeds with the current element excluded, return true *)
+      else
+        check_w_tolerate f (tail @ [h]) lvl system
+
+
+  (* | [] -> false *)
+  (* | [ x ] -> system *)
+  (* | h :: s :: tail -> *)
+  (*     let diff = int_of_string h - int_of_string s in *)
+  (*     if abs diff < 1 || abs diff > 3 then false *)
+  (*     else *)
+  (*       let nlvl = match_level diff in *)
+  (*       if lvl == Neither || nlvl == lvl then *)
+  (*         check (List.append [ s ] tail) nlvl true *)
+  (*       else false *)
+
+(* let rec check_w_tolerate lst lvl system errc = *)
+(*   match lst with *)
+(*   | [] -> false *)
+(*   | [ x ] -> system *)
+(*   | h :: s :: tail -> *)
+(*       let diff = int_of_string h - int_of_string s in *)
+(*       if abs diff < 1 || abs diff > 3 then *)
+(*         if errc == 0 then *)
+(*           check_w_tolerate (List.append [ h ] tail) lvl false (errc + 1) *)
+(*         else false *)
+(*       else *)
+(*         let nlvl = match_level diff in *)
+(*         if lvl == Neither || nlvl == lvl then *)
+(*           check_w_tolerate (List.append [ s ] tail) lvl true errc *)
+(*         else if errc == 0 then *)
+(*           check_w_tolerate (List.append [ h ] tail) lvl false (errc + 1) *)
+(*         else false *)
+
 let rec countl lst c =
   match lst with
   | [] -> c
@@ -44,6 +87,20 @@ let () =
   let content = read file in
 
   let lst = List.map (fun s -> Str.split (Str.regexp "[ \\d]+") s) content in
-  let res = countl (List.map (fun x -> check x Neither false) lst) 0 in
 
-  print_endline (string_of_int res)
+  let r1 = countl (List.map (fun x -> check x Neither false) lst) 0 in
+
+  (* let res = countl (List.map (fun x -> check x Neither false) lst) 0 in *)
+  (* let lst = List.map (fun x -> check_w_tolerate x Neither false 0) lst in *)
+  let r2 = countl (List.map (fun x -> check_w_tolerate check x Neither false) lst) 0 in
+  (* let r2 = countl (List.map (fun x -> begin *) 
+  (*   let safe = check x Neither false in end ) lst) *)
+        (* check x Neither false) lst) 0 in *)
+
+  (* List.iteri (fun idx, x -> print_endline String.concat " : "  (string_of_bool x)) lst *)
+        (* List.iteri (fun idx x -> *) 
+  (* print_endline (String.concat " : " [string_of_int (idx +1); string_of_bool x]) *)
+(* ) lst *)
+
+    print_endline (string_of_int r1);
+    print_endline (string_of_int r2);
