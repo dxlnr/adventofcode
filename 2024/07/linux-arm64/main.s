d2ef4e0eb4a9d2b042a6ad8b3d72dcecd0570ef1
dxlnr <daniel.illner@outlook.de>
2025-06-05 22:45:20 +0000

    update on arm64 linux

--- 2024/07/linux-arm64/main.s
+++ 2024/07/linux-arm64/main.s
@@ -1,6 +1,30 @@
 .global  _start
 .p2align 2
 
+/* open, get size, mmap, write out, clean (munmap & close) */
+_loadf:
+  /* (1) SYSCALL_OPENAT
+  	open and possibly create a file */
+  mov x8, 0x38
+  mov x0, #-100     /* AT_FDCWD */
+  ldr x1, =example
+  mov x2, 0x0  	    /* flags=O_RDONLY */
+  mov x3, 0x0       /* ignored */
+  svc #0
+
+  mov x8, 0x5d  /* SYSCALL_EXIT */
+  svc #0
+
+  /* (3) SYSCALL_MMAP
+	map or unmap files or devices into memory */
+  mov x8, 0xde  
+  mov x0, 0
+  mov x3, 0x1 /* PROT_READ : Page can be read */
+  /* SYSCALL_CLOSE
+  	fd can be closed without invalidating the mapping */
+  mov x8, 0x39
+  mov x0, #1
+
 _printf:
   mov x8, 0x40 /* SYSCALL_WRITE */
   mov x0, #1
@@ -14,11 +38,15 @@ _exit:
   svc #0
 
 _start:
+  b _loadf
   b _printf
   b _exit
 
 .data
+example:
+  .asciz "/home/daniel/code/github/adventofcode/2024/07/example"
 mbuf:
-  .ascii "advent of code 2024(7)"
+  .ascii "Advent of code 2024(7):\n"
 mbuf_len = .-mbuf
+
 
